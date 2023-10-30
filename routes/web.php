7b04527cbb82885b0f57b748464e536b1ed1c6b6
Dekhomarket <dekhomarket2023@gmail.com>
2023-10-30 17:27:36 +0530

    update index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('dashborad', function () {
+    return view('dashborad');
+});
+
+Route::get('index', function () {
+    return view('index');
 });
